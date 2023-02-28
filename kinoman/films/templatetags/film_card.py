38fefbeaa5d18d016aa54ac0cb090204b0457a5b
saalaus <saalaus2@gmail.com>
2023-02-28 18:29:28 +0500

    mainpage view

--- kinoman/films/templatetags/film_card.py
+++ kinoman/films/templatetags/film_card.py
@@ -4,6 +4,5 @@ from films.models import Film
 register = template.Library()
 
 @register.inclusion_tag('films/card.html')
-def show_card(film):
-    context = {'film': Film.objects.get(pk=1)}
-    return context
+def show_card(film: Film):
+    return {'film': film}
--- kinoman/films/views.py
+++ kinoman/films/views.py
@@ -1,6 +1,11 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 from .models import Film
 
-def index(request):
-    context = {'film': Film.objects.get(pk=1)}
-    return render(request, 'films/index.html')
+def index(request: HttpRequest):
+    films = Film.objects.all()
+    chunks = []
+    for i in range(0, len(films), 4):
+        chunks.append(films[i:i+4])
+    context = {'films_chunked': chunks}
+    return render(request, 'films/index.html', context=context)
