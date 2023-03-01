7939f172caefab40867591e86561a41a0ba577db
saalaus <saalaus2@gmail.com>
2023-03-01 08:38:23 +0300

    additional films field

--- kinoman/films/admin.py
+++ kinoman/films/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Film
+from .models import Film, Genre
 
 @admin.register(Film)
 class FilmAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- kinoman/films/models.py
+++ kinoman/films/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+class Genre(models.Model):
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    name = models.CharField(max_length=30, verbose_name='Название')
+
+    def __str__(self) -> str:
+        return self.name
+
 class Film(models.Model):
     class Meta:
         verbose_name = 'Фильм'
@@ -9,6 +19,9 @@ class Film(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
     poster = models.ImageField(upload_to = 'posters', verbose_name='Постер')
+    genre = models.ManyToManyField(Genre)
+    premier_russia = models.DateField(verbose_name='Премьера в России')
+    premier_word = models.DateField(verbose_name='Премьера в мире')
 
     def __str__(self) -> str:
         return self.name
